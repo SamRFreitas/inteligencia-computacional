d2f7cd173d3cd3db3db053635d86ef0fb949116f
samrfreitas <samuelrfreitassi@gmail.com>
2022-10-09 16:10:14 -0300

    Função remover no vetor ordenado inserida

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
+
 import "fmt"
 
-func main () {
-	vetor := [5]int{2, 3, 7, 9, 0}
-	
+func main() {
+	// vetor := [5]int{2, 3, 7, 9, 0}
+	// vetor := [5]int{2, 3, 7, 9, 10}
+	vetor := [5]int{5, 0, 0, 0, 0}
+
 	exibeVetor(vetor)
 
 	fmt.Println(buscaInserir(vetor, 4))
 
-	fmt.Println(buscaRemove(vetor, 3))
+	fmt.Println(buscaRemove(vetor, 10))
+
+	//Tratar quando nao existe esse numero no vetor
+	// remover(vetor, 7)
+
+	inserir(vetor, 2)
 }
 
 func exibeVetor(vetorOrdenado [5]int) {
@@ -17,9 +25,9 @@ func exibeVetor(vetorOrdenado [5]int) {
 
 func buscaInserir(vetorOrdenado [5]int, numero int) int {
 	for index, elemento := range vetorOrdenado {
-		if (numero < elemento || numero == 0) {
-			return index 
-		} 
+		if numero < elemento || elemento == 0 {
+			return index
+		}
 	}
 
 	return -1
@@ -27,10 +35,53 @@ func buscaInserir(vetorOrdenado [5]int, numero int) int {
 
 func buscaRemove(vetorOrdenado [5]int, numero int) int {
 	for index, elemento := range vetorOrdenado {
-		if (numero == elemento) {
-			return index 
-		} 
+		if numero == elemento {
+			return index
+		}
 	}
 
 	return -1
+}
+
+func remover(vetorOrdenado [5]int, numero int) {
+
+	var indiceDeRemocao int = buscaInserir(vetorOrdenado, numero)
+
+	vetorOrdenado[indiceDeRemocao] = 0
+
+	for index, elemento := range vetorOrdenado {
+
+		if index != len(vetorOrdenado)-1 {
+
+			if elemento < vetorOrdenado[index+1] && index >= indiceDeRemocao {
+				vetorOrdenado[index] = vetorOrdenado[index+1]
+				vetorOrdenado[index+1] = 0
+			}
+
+		}
+
+	}
+
+	exibeVetor(vetorOrdenado)
+}
+
+func inserir(vetorOrdenado [5]int, numero int) {
+
+	var indiceDeInsercao int = buscaInserir(vetorOrdenado, numero)
+
+	// for index, elemento := range vetorOrdenado {
+
+	// 	if index != len(vetorOrdenado)-1 {
+
+	// 		if indiceDeInsercao == index {
+	// 			if elemento > vetorOrdenado[index+1] {
+
+	// 			}
+	// 		}
+
+	// 	}
+
+	// }
+
+	exibeVetor(vetorOrdenado)
 }
--- vetores/ordenado.go
+++ vetores/ordenado.go
@@ -8,9 +8,9 @@ func exibeVetor(vetorOrdenado [5]int) {
 
 func buscaInserir(vetorOrdenado [5]int, numero int) int {
 	for index, elemento := range vetorOrdenado {
-		if (numero < elemento || numero == 0) {
-			return index 
-		} 
+		if numero < elemento || numero == 0 {
+			return index
+		}
 	}
 
 	return -1
@@ -18,10 +18,24 @@ func buscaInserir(vetorOrdenado [5]int, numero int) int {
 
 func buscaRemove(vetorOrdenado [5]int, numero int) int {
 	for index, elemento := range vetorOrdenado {
-		if (numero == elemento) {
-			return index 
-		} 
+		if numero == elemento {
+			return index
+		}
 	}
 
 	return -1
+}
+
+func remover(vetorOrdenado [5]int, numero int) {
+
+	var indiceDeRemocao int = buscaRemove(vetorOrdenado, numero)
+
+	for index, _ := range vetorOrdenado {
+		if indiceDeRemocao == index {
+			vetorOrdenado[index] = vetorOrdenado[index+1]
+			vetorOrdenado[index+1] = 0
+		}
+	}
+
+	exibeVetor(vetorOrdenado)
 }
