738bcb70a94c4809594a3cb3d7b16172e7ed6f61
samrfreitas <samuelrfreitassi@gmail.com>
2022-10-12 14:47:20 -0300

    Refatorando codigos em pacotes

--- vetores/naoOrdenado.go
+++ vetores/naoOrdenado.go
@@ -1,72 +1,64 @@
 package main
+
 import "fmt"
-func iniciarVetor(vetor [6]int) {
-	fmt.Println("Iniciar Vetor")
-	imprimeVetor(vetor)
+
+func IniciarVetor(vetor [6]int) {
+
 	for index, _ := range vetor {
 		vetor[index] = -1
 	}
-	imprimeVetor(vetor)
-	fmt.Println("------------------------------")
+
 }
 
-func imprimeVetor(vetor [6]int) {
-	fmt.Println(vetor)
-	fmt.Println()
-}
+func BuscaLinear(vetor [6]int, numero int) int {
 
-func buscaLinear(vetor [6]int, numero int) int {
 	for index, num := range vetor {
-		if (num == numero) {
-			return index 
-		} 
+		if num == numero {
+			return index
+		}
 	}
 
 	return -1
 }
 
-func removerNumero(vetor [6]int, numero int) {
+func RemoverNumero(vetor [6]int, numero int) {
 	fmt.Printf("Remover número %d Vetor: ", numero)
-	imprimeVetor(vetor)
 
-	var resultadoBuscaLinear int = buscaLinear(vetor, numero)
+	var resultadoBuscaLinear int = BuscaLinear(vetor, numero)
 
-	if(resultadoBuscaLinear != -1){
+	if resultadoBuscaLinear != -1 {
 		vetor[resultadoBuscaLinear] = -1
 	} else {
 		fmt.Println("Número não existente no vetor")
 	}
 
-	imprimeVetor(vetor)
 	fmt.Println("------------------------------")
 	fmt.Println()
 }
 
-func inserirNumero(vetor [6]int, numero int) {
+func InserirNumero(vetor [6]int, numero int) {
 	fmt.Printf("Inserir número %d no Vetor: ", numero)
-	imprimeVetor(vetor)
 
 	var temEspaco bool = false
 
-	if(buscaLinear(vetor, numero) == -1){
+	if BuscaLinear(vetor, numero) == -1 {
 
 		for index, num := range vetor {
-			if(num == -1) {
+			if num == -1 {
 				vetor[index] = numero
 				temEspaco = true
 				break
 			}
 		}
 
-	} else{
+	} else {
 		fmt.Println("Número já existente no vetor")
 	}
 
-	if (!temEspaco && buscaLinear(vetor, numero) == -1) {
+	if !temEspaco && BuscaLinear(vetor, numero) == -1 {
 		fmt.Println("Vetor Sem Espaço")
 	}
 
-	imprimeVetor(vetor)
 	fmt.Println("------------------------------")
 	fmt.Println()
 }
--- vetores/ordenado.go
+++ vetores/ordenado.go
@@ -2,13 +2,14 @@ package main
 
 import "fmt"
 
-func exibeVetor(vetorOrdenado [5]int) {
+func ExibeVetor(vetorOrdenado [5]int) {
 	fmt.Println(vetorOrdenado)
 }
 
-func buscaInserir(vetorOrdenado [5]int, numero int) int {
+func BuscaIndiceDeInsercao(vetorOrdenado [5]int, numero int) int {
 	for index, elemento := range vetorOrdenado {
-		if numero < elemento || numero == 0 {
+		if numero < elemento || elemento == 0 {
+
 			return index
 		}
 	}
@@ -16,7 +17,7 @@ func buscaInserir(vetorOrdenado [5]int, numero int) int {
 	return -1
 }
 
-func buscaRemove(vetorOrdenado [5]int, numero int) int {
+func BuscaIndiceDeRemocao(vetorOrdenado [5]int, numero int) int {
 	for index, elemento := range vetorOrdenado {
 		if numero == elemento {
 			return index
@@ -26,16 +27,68 @@ func buscaRemove(vetorOrdenado [5]int, numero int) int {
 	return -1
 }
 
-func remover(vetorOrdenado [5]int, numero int) {
+func Remover(vetorOrdenado [5]int, numero int) {
 
-	var indiceDeRemocao int = buscaRemove(vetorOrdenado, numero)
+	var indiceDeRemocao int = BuscaIndiceDeRemocao(vetorOrdenado, numero)
 
-	for index, _ := range vetorOrdenado {
-		if indiceDeRemocao == index {
-			vetorOrdenado[index] = vetorOrdenado[index+1]
-			vetorOrdenado[index+1] = 0
+	vetorOrdenado[indiceDeRemocao] = 0
+
+	for index, elemento := range vetorOrdenado {
+
+		if index != len(vetorOrdenado)-1 {
+
+			if elemento < vetorOrdenado[index+1] && index >= indiceDeRemocao {
+				vetorOrdenado[index] = vetorOrdenado[index+1]
+				vetorOrdenado[index+1] = 0
+			}
+
+		}
+
+	}
+
+	ExibeVetor(vetorOrdenado)
+}
+
+func ContarQuantidadeDeNumeroInseridos(vetorOrdenado [5]int) int {
+	var quantidadeDeNumerosInseridosNoVetor int = 0
+
+	for _, element := range vetorOrdenado {
+		if element > 0 {
+			quantidadeDeNumerosInseridosNoVetor++
 		}
 	}
 
-	exibeVetor(vetorOrdenado)
+	return quantidadeDeNumerosInseridosNoVetor
+}
+
+func VerificarSeTemEspacoNoVetor(quantidadeDeNumerosInseridosNoVetor int, vetorOrdenado [5]int) bool {
+
+	if quantidadeDeNumerosInseridosNoVetor == len(vetorOrdenado) {
+		return false
+	} else {
+		return true
+	}
+}
+
+func Inserir(vetorOrdenado [5]int, numero int) [5]int {
+
+	var indiceDeInsercao int = BuscaIndiceDeInsercao(vetorOrdenado, numero)
+
+	var i int = len(vetorOrdenado) - 1
+
+	for i > 0 {
+
+		if i >= indiceDeInsercao {
+			// fmt.Println(vetorOrdenado[i])
+			vetorOrdenado[i] = vetorOrdenado[i-1]
+		}
+
+		i--
+	}
+
+	vetorOrdenado[indiceDeInsercao] = numero
+
+	ExibeVetor(vetorOrdenado)
+
+	return vetorOrdenado
 }
